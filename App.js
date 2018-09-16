61ae9dba4103e6a14b02050039ca5594e72b995c
yacaFx <sergiobritor@gmail.com>
2018-09-16 14:36:12 -0400

    Add teams list

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View } from "react-native";
+import { List, ListItem } from "react-native-elements";
 
 const equipos = [
   {
@@ -51,6 +52,12 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <View />;
+    return (
+      <List>
+        {equipos.map(equipo => (
+          <ListItem key={equipo.id} title={equipo.nombre} />
+        ))}
+      </List>
+    );
   }
 }
