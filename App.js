ee232d1675d476b4faf88ab6c3fba820814e2222
yacaFx <sergiobritor@gmail.com>
2018-09-16 12:26:02 -0400

    Clean App

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-elements";
+import { View } from "react-native";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hola Mundo</Text>
-
-        <Button
-          title="Mi botón"
-          raised
-          icon={{ name: "cached" }}
-          backgroundColor="blue"
-        />
-
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <View />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
